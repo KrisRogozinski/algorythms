7b61b54ee5d720721753afa70fa93ae593a5a82a
Krzysztof Rogoziński <krzysztof.rogozinski@makolab.net>
2017-10-16 11:08:33 +0100

    File cleanup - final solution

--- ConsecutiveStrings.js
+++ ConsecutiveStrings.js
@@ -1,26 +1,4 @@
 function longestConsec(strarr, k) {
-    let n = strarr.length,
-        arr = [],
-        result = "";
-    if (n == 0 || k > n || k <= 0)
-      return "";
-
-    for (let i = 0; i < n; i++){
-      arr.push(strarr[i].length);
-    }
-    console.log(arr);
-    arr = arr.sort(function(a, b){
-      return b - a;
-    });
-    console.log(arr);
-    for (let i = 0; i < k-1; i++){
-
-      result += strarr[arr[i]];
-    }
-    return result;
-}
-
-function longestConsec2(strarr, k) {
   let rslt = '';
   if (k <= 0 || k > strarr.length)
     return rslt;
@@ -32,6 +10,3 @@ function longestConsec2(strarr, k) {
   }
   return rslt;
 }
-
-
-console.log(longestConsec2(["zo", "abigail", "the", "form", "libe", "zas"], 2));
